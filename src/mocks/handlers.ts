1a19f5e663bcd9ef5829701788dfaf68f290fabc
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-07 17:55:34 +0100

    Add MSW dependency and mock files and handlers

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,8 @@
+import { http, HttpResponse } from "msw";
+import { charactersMock } from "./charactersMock";
+
+export const handlers = [
+  http.get(import.meta.env.VITE_API_URL, () => {
+    return HttpResponse.json(charactersMock);
+  }),
+];
