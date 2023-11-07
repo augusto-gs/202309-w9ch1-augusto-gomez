7ec002373da92267a13e81cd2be4db10b40381c4
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-07 12:02:23 +0100

    Add FilmsPage functionality to fill cards

--- src/pages/FilmsPage.tsx
+++ src/pages/FilmsPage.tsx
@@ -1,8 +1,36 @@
+import { RootState } from "../store";
+import { useAppSelector } from "../store/hooks";
+import useMovieApi from "../hooks/useMoviesApi";
+import { useEffect } from "react";
+import { loadFilms } from "../store/features/slices";
+import { useDispatch } from "react-redux";
+
 const FilmsPage = (): React.ReactElement => {
+  const { getFilms } = useMovieApi();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const currentFilms = await getFilms();
+      dispatch(loadFilms(currentFilms));
+    })();
+  }, [dispatch, getFilms]);
+
+  const films = useAppSelector((state: RootState) => {
+    return state.films;
+  });
+
   return (
     <>
       <h1 className="list-title">Movies list</h1>
-      <ul className="movies-list"></ul>
+      <ul className="movies-list">
+        {films.films.map((movie) => (
+          <li>
+            {movie.id} {movie.name} {movie.year}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
