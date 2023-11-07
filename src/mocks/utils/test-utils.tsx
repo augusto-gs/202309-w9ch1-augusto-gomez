6851720ee37ab5fdd703366da4b689c2df4beefd
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-07 20:33:16 +0100

    Add testing suite for FilmsPage component

---
+++ src/mocks/utils/test-utils.tsx
@@ -0,0 +1,41 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import type { PreloadedState } from "@reduxjs/toolkit";
+import { moviesReducer } from "../../store/features/slices";
+import React, { PropsWithChildren } from "react";
+import { render } from "@testing-library/react";
+import type { RenderOptions } from "@testing-library/react";
+import { Provider } from "react-redux";
+
+const rootReducer = combineReducers({
+  films: moviesReducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) => {
+  const Wrapper = ({ children }: PropsWithChildren): JSX.Element => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
