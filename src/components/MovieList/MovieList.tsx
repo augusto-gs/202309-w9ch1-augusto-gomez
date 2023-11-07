446ef2ccec69af4361afe1dc845703a8f07fbdf3
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-07 21:13:29 +0100

    Create MovieList component from different component structure

---
+++ src/components/MovieList/MovieList.tsx
@@ -0,0 +1,36 @@
+import { RootState } from "../../store";
+import { useAppSelector } from "../../store/hooks";
+import useMovieApi from "../../hooks/useMoviesApi";
+import { useEffect } from "react";
+import { loadFilms } from "../../store/features/slices";
+import { useDispatch } from "react-redux";
+import Films from "../../store/features/types";
+import MovieCard from "../MovieCard/MovieCard";
+import MovieListStyled from "./MovieListStyled";
+
+const MovieList = (): React.ReactElement => {
+  const { getFilms } = useMovieApi();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const currentFilms = await getFilms();
+      dispatch(loadFilms(currentFilms));
+    })();
+  }, [dispatch, getFilms]);
+
+  const films = useAppSelector((state: RootState) => state.films);
+
+  return (
+    <MovieListStyled>
+      {films.films.map((movie: Films) => (
+        <li className="movies-list__item" key={movie.id}>
+          <MovieCard movie={movie} />
+        </li>
+      ))}
+    </MovieListStyled>
+  );
+};
+
+export default MovieList;
---
+++ src/components/MovieList/MovieListStyled.ts
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+const MovieListStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .list-title {
+    text-align: center;
+    font-size: 50px;
+  }
+`;
+
+export default MovieListStyled;
