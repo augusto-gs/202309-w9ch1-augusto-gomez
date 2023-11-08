ecf777bf1fbba7b256fd836d4dd7652d684c0d49
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-08 12:01:43 +0100

    Add changes to GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store";
-import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import { store } from "./store";
 import mainTheme from "./styles/mainTheme";
+import App from "./components/App/App";
+import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
