0d955ce39a466d9976beb43e832101fbd4f35975
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-09 17:27:00 +0100

    Add FormPage component and routes to navigate webpage

---
+++ src/pages/FormPage/FormPage.tsx
@@ -0,0 +1,9 @@
+const FormPage = () => {
+  return (
+    <section className="form-page">
+      <h2 className="form-title">Create your own movie</h2>
+    </section>
+  );
+};
+
+export default FormPage;
